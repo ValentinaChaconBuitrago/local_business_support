f66eefff870db1b6b54787782fc56195276dbd73
Juan arango <jd.arango@uniandes.edu.co>
2020-04-11 02:56:10 -0500

    con propia location e iconitos

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Questions from "./components/Questions.js";
 import Maps from "./components/Maps.js";
+import { geolocated } from "react-geolocated";
 
 let places = [];
+let latActual = 0;
+let lonActual = 0;
+
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
@@ -16,6 +20,13 @@ function App() {
   // this useEffect will run once
   // similar to componentDidMount()
   useEffect(() => {
+    const geo = navigator.geolocation.getCurrentPosition(function success(pos) {
+      var crd = pos.coords;
+      latActual = crd.latitude;
+      lonActual = crd.longitude;
+      console.log(latActual, "LARTTTT");
+    });
+
     fetch("./getRestaurants")
       .then((res) => res.json())
       .then(
@@ -36,7 +47,7 @@ function App() {
     fetch("/getUser")
       .then((res) => res.json())
       .then((user) => setUser(user));
-  }, []);
+  }, [latActual, lonActual]);
 
   if (error) {
     return <div>Error: {error.message}</div>;
@@ -45,34 +56,68 @@ function App() {
   } else {
     return (
       <div>
-        <div>
-          <div className="container">
-            <h1>Hola</h1>
-          </div>
-        </div>
-        <div className="row">
-          <div id="storesDiv" className="col-md-6">
-            <h1>stores</h1>
-            <ul>
-              {items.map((item) => (
-                <li key={item.name}>{item.name}</li>
-              ))}
+        <nav class="navbar navbar-inverse">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#">
+                WebSiteName
+              </a>
+            </div>
+            <ul class="nav navbar-nav">
+              <li class="active">
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Page 1</a>
+              </li>
+              <li>
+                <a href="#">Page 2</a>
+              </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li>
+                <a href="#">
+                  <span class="glyphicon glyphicon-user"></span> Sign Up
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span class="glyphicon glyphicon-log-in"></span> Login
+                </a>
+              </li>
             </ul>
           </div>
+        </nav>
+        <div>
+          <div>
+            <div className="container"></div>
+          </div>
 
-          <div className="col-md-6">
-            <div id="rowDiv">
-              <h1>Mapa</h1>
-              <Maps
-                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCIBfNAk_YO6MsSe2mu2JAg-Voc6bDbItU&libraries=geometry,drawing,places"
-                loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={
-                  <div style={{ height: `100%`, width: `100%` }} />
-                }
-                mapElement={<div style={{ height: `80vh` }} />}
-                mar={{ lat: 4.7106502, lng: -74.0493288 }}
-                tiendas={items}
-              ></Maps>
+          <div className="row">
+            <div id="storesDiv" className="col-md-6">
+              <h1 id="storesTitle">stores</h1>
+              <ul>
+                {items.map((item) => (
+                  <li key={item.name}>{item.name}</li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="col-md-6">
+              <div id="rowDiv">
+                <h1>Mapa</h1>
+                <Maps
+                  isMarkerShown
+                  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCIBfNAk_YO6MsSe2mu2JAg-Voc6bDbItU&libraries=geometry,drawing,places"
+                  loadingElement={<div style={{ height: `100%` }} />}
+                  containerElement={
+                    <div style={{ height: `100%`, width: `100%` }} />
+                  }
+                  mapElement={<div style={{ height: `80vh` }} />}
+                  mar={{ lat: latActual, lng: lonActual }}
+                  tiendas={items}
+                ></Maps>
+              </div>
             </div>
           </div>
         </div>
--- front/src/components/Maps.js
+++ front/src/components/Maps.js
@@ -6,22 +6,28 @@ import {
   InfoWindow,
 } from "react-google-maps";
 import React, { useState, useEffect } from "react";
+let latActual = 0;
+let lonActual = 0;
 
 const Maps = withGoogleMap((props) => {
   const [selected, setSelected] = useState(null);
-  console.log("tiendas", props.tiendas);
+
   return (
-    <GoogleMap
-      defaultZoom={17}
-      defaultCenter={{ lat: 4.7106502, lng: -74.0493288 }}
-    >
+    <GoogleMap defaultZoom={14} defaultCenter={props.mar}>
       {props.isMarkerShown && <Marker position={props.mar} />}
       {props.tiendas.map((tienda) => (
         <Marker
+          icon={{
+            url:
+              " https://maps.gstatic.com/mapfiles/ms2/micons/grocerystore.png",
+          }}
           key={tienda._id}
           position={tienda.position}
           onMouseOver={() => {
             setSelected(tienda);
+          }}
+          onMouseOut={() => {
+            setSelected(null);
           }}
         />
       ))}
@@ -37,7 +43,7 @@ const Maps = withGoogleMap((props) => {
             <img
               id="imgGlobo"
               src={selected.image}
-              width="80"
+              height="80"
               alt="Imagen´{selected.name}´"
             ></img>
           </div>
